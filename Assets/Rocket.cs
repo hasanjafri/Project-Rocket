00bb31bd3a74fad1d21c1d88b13e85c987c4927e
Hasan Jafri <hasan.jafri7@gmail.com>
2018-01-15 00:23:16 -0500

    Add thrusting and rotation to rocket

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour {
 
+    Rigidbody rigidBody;
+
 	// Use this for initialization
 	void Start () {
-		
+        rigidBody = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
@@ -16,13 +18,13 @@ public class Rocket : MonoBehaviour {
 
     void ProcessInput() {
         if (Input.GetKey(KeyCode.Space)) {
-            print("Thrusting...");
+            rigidBody.AddRelativeForce(Vector3.up);
         }
 
         if (Input.GetKey(KeyCode.A)) {
-            print("Rotating Left...");
+            transform.Rotate(Vector3.forward);
         } else if (Input.GetKey(KeyCode.D)) {
-            print("Rotating Right...");
+            transform.Rotate(-Vector3.forward);
         }
     }
 }
