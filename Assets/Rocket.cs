4511c637bdb74a1827f3cf1238c0b6f736ca041f
Hasan Jafri <hasan.jafri7@gmail.com>
2018-01-15 23:34:37 -0500

    Add thrusting sound effect, fix rotations, added a MVP for level 1

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -4,27 +4,64 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour {
 
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
+
     Rigidbody rigidBody;
+    AudioSource audioSource;
 
 	// Use this for initialization
 	void Start () {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        ProcessInput();
+        Thrust();
+        Rotate();
 	}
 
-    void ProcessInput() {
-        if (Input.GetKey(KeyCode.Space)) {
-            rigidBody.AddRelativeForce(Vector3.up);
+    void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        switch (collision.gameObject.tag) {
+            case "Friendly":
+                break;
+            default:
+                print("Death");
+                break;
+        }
+    }
+
+    void Rotate()
+    {
+        rigidBody.freezeRotation = true; // take manual control of rotation
+        float rotationSpeed = rcsThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationSpeed);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationSpeed);
         }
 
-        if (Input.GetKey(KeyCode.A)) {
-            transform.Rotate(Vector3.forward);
-        } else if (Input.GetKey(KeyCode.D)) {
-            transform.Rotate(-Vector3.forward);
-        }
+        rigidBody.freezeRotation = false;
     }
 }
